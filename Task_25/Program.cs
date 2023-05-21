1ff76d8d6efaa162201962f097eb005a65390de1
Pavel <alopavlo@mail.ru>
2023-05-21 16:48:41 +0300

    Задача 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и
+//  возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Number()
+{
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int Degree()
+{
+    Console.WriteLine("Введите степень: ");
+    int deg = int.Parse(Console.ReadLine());
+    return deg;
+}
+int num = Number();
+int deg = Degree();
+
+Console.Write($"Число {num} в степени {deg} равняется {Math.Pow(num, deg)}");
