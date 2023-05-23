791c2f92bad666717a28aad807f57fb8c6975f02
Pavel <alopavlo@mail.ru>
2023-05-23 19:48:10 +0300

    Задача 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 4, 76, 3 -> [1, 2, 5, 7, 19,4,76,3]
+
+// 6, 1, 33, 43, 12, 3 ,2, 9 -> [6, 1, 33,43,12,3,2,9]
+
+
+int[] numbers = new int[8];
+for(int i=0; i<numbers.Length; i++)
+{
+    Random array = new Random();
+    numbers[i] = Convert.ToInt32(array.Next(-5, 15));
+    Console.WriteLine("El: " +numbers[i]);
+}
